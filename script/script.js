2b0f7cf329177b178b466871c272f7443143447a
SnowStriX <secondex@mail.ru>
2024-03-28 01:54:03 +0400

    commit first

--- script/script.js
+++ script/script.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 $(".option").click(function(){
     $(".option").removeClass("active");
     $(this).addClass("active");
  });
-=======
-alert("sffhlsf");
->>>>>>> parent of 5436e73 (Aboba)
+
